10acf006bd5a1707870ba29d40ef865aa22849b8
Ivan <capobianco.ivan@gmail.com>
2021-09-16 17:56:58 +0200

    feat: post /update route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import protobuf from "protobufjs";
+import { DeviceUpdateMsg } from "./protobufClasses/device/update";
 
 const app = express();
 
-app.post("/update", (_, res) => {
-  // console.log(req.body);
-  res.send({ message: "nothing" });
+app.post("/update", express.raw(), (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const payload = new Uint8Array(req.body);
+
+    if (payload.length <= 0) {
+      res.send({ error: "no body" });
+      return next();
+    }
+
+    const msg = DeviceUpdateMsg.decode(payload);
+
+    res.send({ deviceId: msg.deviceId });
+  } catch (e) {
+    if (e instanceof protobuf.util.ProtocolError) {
+      // e.instance holds the so far decoded message with missing required fields
+      res.send({ error: "required fields are missing" });
+    } else {
+      // wire format is invalid
+      res.send({ error: "invalid format" });
+    }
+  }
 });
 
 export default app;
