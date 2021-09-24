f8850c92128fc2f24d37eee70ade1eb7e744c69b
Ivan <capobianco.ivan@gmail.com>
2021-09-24 11:15:41 +0200

    feat: error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
 import api from "./api/api";
+import errorHandler from "./middlewares/errorHandler";
 import notFound from "./middlewares/notFound";
 
 const app = express();
@@ -7,5 +8,7 @@ const app = express();
 app.use("/api", api);
 
 app.use(notFound);
+
+app.use(errorHandler as ErrorRequestHandler);
 
 export default app;
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
+
+interface errorToSend {
+  name: string;
+  message: string;
+}
+
+const errorHandler: ErrorRequestHandler = (
+  error: Error,
+  _request: Request,
+  response: Response,
+  _next: NextFunction
+) => {
+  const statusCode = response.statusCode === 200 ? 500 : response.statusCode;
+  response.status(statusCode);
+
+  const er: errorToSend = {
+    name: error.name,
+    message: error.message,
+  };
+
+  response.json(er);
+};
+
+export default errorHandler;
