9c51fce5960b21662f2bb2dd1176a489f2ef5d26
Ivan <capobianco.ivan@gmail.com>
2021-09-24 14:29:19 +0200

    feat: create sensor route

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import device from "./device/device.routes";
+import sensor from "./sensor/sensor.routes";
 import sensorData from "./sensorData/sensorData.routes";
 
 const router = express.Router();
 
 router.use("/device", device);
+router.use("/sensor", sensor);
 router.use("/sensor_data", sensorData);
 
 export default router;
---
+++ src/api/sensor/sensor.routes.ts
@@ -0,0 +1,22 @@
+import express, { NextFunction, Request, Response } from "express";
+import prisma from "../../db/db";
+
+const router = express.Router();
+
+router.post("/create", express.json(), async (req: Request, res: Response, next: NextFunction) => {
+  const { deviceId }: { deviceId: number } = req.body;
+
+  try {
+    const sensor = await prisma.sensor.create({
+      data: {
+        device_id: deviceId,
+      },
+    });
+    res.status(201).send(sensor);
+  } catch (e) {
+    res.status(400);
+    return next(e);
+  }
+});
+
+export default router;
